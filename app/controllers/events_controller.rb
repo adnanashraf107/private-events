20e86de6a996b706ea03b2e80cfbd6d9d77f4d22
adnanashraf107 <adnan.ashraf@thededen.co>
2019-07-29 15:24:39 +0500

    Event create action added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,25 @@
 class EventsController < ApplicationController
+
+  def new
+    @creator = User.find(current_user.id)
+    @event = Event.new
+  end
+
+  def create
+    @creator = User.find(current_user.id)
+    @event = Event.new(validate_event_params)
+    @event.creator = @creator
+    if @event.save
+      flash.notice = 'Event created successfully.'
+      redirect_to user_index_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def validate_event_params
+      params.require(:event).permit(:title, :location, :date)
+    end
+  
 end
