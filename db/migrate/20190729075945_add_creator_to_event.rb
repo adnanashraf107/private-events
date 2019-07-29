a74808cc1f2dfbb3dda6372a4af8ee8c043a3322
adnanashraf107 <adnan.ashraf@thededen.co>
2019-07-29 13:09:47 +0500

    User Added To Events

---
+++ db/migrate/20190729075945_add_creator_to_event.rb
@@ -0,0 +1,6 @@
+class AddCreatorToEvent < ActiveRecord::Migration[6.0]
+  def change
+    add_column :events, :user_id, :integer
+    add_foreign_key :events, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_074128) do
+ActiveRecord::Schema.define(version: 2019_07_29_075945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_074128) do
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_07_29_074128) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
