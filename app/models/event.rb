59a51a662ca962bbcc3533b200348df7b037c37d
adnanashraf107 <adnan.ashraf@thededen.co>
2019-07-29 18:05:21 +0500

    Attendace Model Added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
 
-  belongs_to :creator, class_name: "User", foreign_key: 'user_id'
-
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  has_many :attendance
+  has_many :attendee, class_name: "User", through: :attendance
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
+  has_many :attendance
+  has_many :attended_events, class_name: "Event", through: :attendance
 
 end
