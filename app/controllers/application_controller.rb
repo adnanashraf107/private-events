fbf41a69721afd068794c3b9b52faf9caf09f707
adnanashraf107 <adnan.ashraf@thededen.co>
2019-07-30 11:46:04 +0500

    Name added in devise registration action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   respond_to :html, :json
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || user_index_path
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
